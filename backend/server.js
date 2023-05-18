f577c54ac071668dbf02eda6952bb9f0601c33a9
SadeEtensh <eteneshsade550305@gmail.com>
2023-05-18 09:08:20 +0300

    Backend organization completed

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
+const port = process.env.PORT || 4000;
+
+const app = express();
+
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => res.send("Server is ready"));
+
+app.use(notFound);
+app.use(errorHandler);
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
+// /api/v1/resources_name
+
+//endpoints
+//- **POST /api/users** - Register a User
+//- **POST /api/users/auth** -Authenticate a user and get token
+//- **POST /api/users/logout** - Logout user and clear cookie
+//- **GET /api/users/profile** - Get user profile
+//- **PUT /api/users/profile** - update profilr
