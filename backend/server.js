5c192a62d25a43a7da786cf7f34901a839085d9a
SadeEtensh <eteneshsade550305@gmail.com>
2023-05-18 10:10:28 +0300

    MongoDB database connection completed

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,14 @@ import dotenv from "dotenv";
 dotenv.config();
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-
+import connectDB from "./config/db.js";
 const port = process.env.PORT || 4000;
 
+connectDB();
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("Server is ready"));
