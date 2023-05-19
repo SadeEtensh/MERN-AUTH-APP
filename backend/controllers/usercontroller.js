4562b12d38ee64d237247f4d8e32ec2b77efce48
SadeEtensh <eteneshsade550305@gmail.com>
2023-05-19 12:56:08 +0300

    Login Functionality updated

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -1,9 +1,25 @@
 import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 //@desc Auth user/set token
 //route POST /api/users/auth
-//@access Public
+//@access Public ---Login functionality
 const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
   res.status(200).json({ message: "Auth User" });
 });
 
@@ -27,6 +43,7 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 
   if (user) {
+    generateToken(res, user._id);
     res.status(201).json({
       _id: user._id,
       name: user.name,
